0de62f5aabefdb0bf03bc5ff5b89e3a869fd80cb
Marius Vuscan <marius.vuscan@gmail.com>
2023-12-19 20:48:16 +0200

    Added service principal creation

---
+++ docs/service-principal-creation.md
@@ -0,0 +1,9 @@
+# Intro
+- In order to execute the Terraform operations described in the git repository from a Github pipeline, we first need to authorize Terraform to execute actions to our Azure cluster. The chosen authorization method was the service principal.
+- The service principal configuration was done using a Terraform script that will be ran locally.
+
+# Steps
+- go inside the service-principal-creation folder: **infra\modules\service-principal-creation**
+- run the init command: ```terraform init```
+- replace the subscription-id placeholder and run the plan command: ```terraform plan -out sp.tfplan -var="subscription_id=<subscription-id>"```
+- replace the subscription-id placeholder and run the apply command: ```terraform apply -var="subscription_id=<subscription-id>"```
---
+++ infra/modules/service-principal-creation/main.tf
@@ -0,0 +1,88 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.85.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.47.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {
+}
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
+resource "azuread_application" "app" {
+  display_name = "ms-assignment"
+}
+
+resource "azuread_service_principal" "app" {
+  client_id = azuread_application.app.client_id
+}
+
+resource "azuread_service_principal_password" "app" {
+  service_principal_id = azuread_service_principal.app.object_id
+  end_date_relative    = "800h"
+}
+
+resource "azurerm_role_definition" "aks_role" {
+  name        = "Custom_AKS_Operator"
+  description = "Custom role for AKS operator"
+  scope       = "/subscriptions/${var.subscription_id}"
+  permissions {
+    actions = [
+      "Microsoft.ContainerService/managedClusters/read",
+      "Microsoft.ContainerService/managedClusters/write",
+      "Microsoft.ContainerService/managedClusters/delete",
+    ]
+    not_actions = []
+  }
+}
+
+resource "azurerm_role_definition" "acr_role" {
+  name        = "Custom_ACR_Operator"
+  description = "Custom role for ACR operator"
+  scope       = "/subscriptions/${var.subscription_id}"
+  permissions {
+    actions = [
+      "Microsoft.ContainerRegistry/registries/read",
+      "Microsoft.ContainerRegistry/registries/write",
+      "Microsoft.ContainerRegistry/registries/delete",
+      "Microsoft.ContainerRegistry/registries/push/write",
+      "Microsoft.ContainerRegistry/registries/listCredentials/action",
+    ]
+    not_actions = []
+  }
+}
+
+resource "azurerm_role_assignment" "aks_role_assignment" {
+  scope                = "/subscriptions/${var.subscription_id}"
+  role_definition_name = azurerm_role_definition.aks_role.name
+  principal_id         = azuread_service_principal.app.id
+}
+
+resource "azurerm_role_assignment" "acr_role_assignment" {
+  scope                = "/subscriptions/${var.subscription_id}"
+  role_definition_name = azurerm_role_definition.acr_role.name
+  principal_id         = azuread_service_principal.app.id
+}
+
+output "client_id" {
+  value = azuread_service_principal.app.client_id
+}
+
+output "client_secret" {
+  value     = azuread_service_principal_password.app.value
+  sensitive = true
+}
