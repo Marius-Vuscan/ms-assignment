8c5499d1d256f493e11465aa2f98be39425fcb81
Marius Vuscan <marius.vuscan@gmail.com>
2023-12-20 10:06:33 +0200

    Updated acr config

--- infra/modules/resources-configuration/container-registry.tf
+++ infra/modules/resources-configuration/container-registry.tf
@@ -28,7 +28,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "ms-assignment-acr"
+  name                = "assignmentacr"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
