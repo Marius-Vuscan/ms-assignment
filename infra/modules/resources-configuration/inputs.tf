6248f34dca66799a0769dd8dbafd54d9d9e4ae23
Marius Vuscan <marius.vuscan@gmail.com>
2023-12-20 20:03:36 +0200

    Structure adjustments

---
+++ infra/modules/resources-configuration/inputs.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "The Azure region for resources"
+  default     = "West Europe"
+}
+
+variable "resource_group_name" {
+  description = "The name of the Azure Resource Group"
+  default     = "ms-assignment"
+}
---
+++ infra/modules/resources-configuration/terraform.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.85.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "5.42.0"
+    }
+  }
+
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "github" {
+}
