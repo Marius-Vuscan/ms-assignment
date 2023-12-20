c92390c130887ed58e336fe3deade252b1d2f7e7
Marius Vuscan <marius.vuscan@gmail.com>
2023-12-20 11:57:20 +0200

    github secrets

--- infra/modules/resources-configuration/container-registry.tf
+++ infra/modules/resources-configuration/container-registry.tf
@@ -42,19 +42,19 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled = false
 }
 
-resource "github_actions_secret" "example_secret" {
+resource "github_actions_secret" "acr_login_server" {
   repository      = "ms-assignment"
   secret_name     = "ACR_LOGIN_SERVER"
   plaintext_value = azurerm_container_registry.acr.login_server
 }
 
-resource "github_actions_secret" "example_secret" {
+resource "github_actions_secret" "acr_username" {
   repository      = "ms-assignment"
   secret_name     = "ACR_USERNAME"
   plaintext_value = azurerm_container_registry.acr.admin_username
 }
 
-resource "github_actions_secret" "example_secret" {
+resource "github_actions_secret" "acr_password" {
   repository      = "ms-assignment"
   secret_name     = "ACR_PASSWORD"
   plaintext_value = azurerm_container_registry.acr.admin_password
