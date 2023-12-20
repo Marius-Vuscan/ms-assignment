2ca5c6573cb5fe24ff85616c830319c903e9f407
Marius Vuscan <marius.vuscan@gmail.com>
2023-12-20 20:04:39 +0200

    Structure adjustments

--- infra/modules/resources-configuration/container-registry.tf
+++ infra/modules/resources-configuration/container-registry.tf
@@ -24,16 +24,3 @@ resource "github_actions_secret" "acr_password" {
   secret_name     = "ACR_PASSWORD"
   plaintext_value = azurerm_container_registry.acr.admin_password
 }
-
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
-
-output "acr_username" {
-  value = azurerm_container_registry.acr.admin_username
-}
-
-output "acr_password" {
-  value     = azurerm_container_registry.acr.admin_password
-  sensitive = true
-}
