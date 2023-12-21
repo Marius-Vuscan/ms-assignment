c4b70a22da958c0b858d2a7a69fa9c7ffcce64ee
Marius Vuscan <marius.vuscan@gmail.com>
2023-12-21 12:27:28 +0200

    Fixed role name

--- infra/modules/resources-configuration/aks.tf
+++ infra/modules/resources-configuration/aks.tf
@@ -1,6 +1,6 @@
 resource "azurerm_role_assignment" "aks_acrpull" {
   scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AksAcrPull"
+  role_definition_name = "AcrPull"
   principal_id         = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity.0.object_id
 }
 
